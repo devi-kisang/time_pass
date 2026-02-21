6fcf2edf253ebb6ee95e80cc3eb68d0d69fae410
devi-kisang <devi.kisang@gmail.com>
2026-02-21 18:55:08 +0530

    Added metrics endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,13 @@ from fastapi import FastAPI
 from app.api.health import router as health_router
 from app.api.movies import router as movies_router
 
+from prometheus_fastapi_instrumentator import Instrumentator
+
 app = FastAPI(title="TIME-PASS API")
 
 app.include_router(health_router, prefix="/api")
 app.include_router(movies_router, prefix="/api")
+
+
+# Enable metrics
+Instrumentator().instrument(app).expose(app)
